2dcfdf01a8beca0876c8a43cc14bb20fd450dc8f
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-03-11 00:27:14 +0530

    add

--- service.js
+++ service.js
@@ -1,4 +1,14 @@
-// const queryStrings ={
-//     app_id: process.env.REACT_APP_ID,
-//     app_key:process.env.REACT_APP_APP_KEY
-// }
+const queryStrings ={
+    app_id: process.env.REACT_APP_ID,
+    app_key:process.env.REACT_APP_APP_KEY
+}
+export const fetchData = async (defalutQuery) => {
+    const {app_id , app_key} = queryString;
+    try {
+        const data = await fetch()
+    }
+    catch(e){
+        console.log(e, 'something went wrong')
+        return e
+    }
+}
